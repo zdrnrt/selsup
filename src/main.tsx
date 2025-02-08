a2ba1a4aefe67c4b32a20dd02c2b3235e3ef0681
Задорин Артем <artfan95@gmail.com>
2025-02-08 23:50:07 +0700

    результат

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import React, { StrictMode } from 'react'
+import React, { ReactElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'normalize.css'
+import './main.css'
 
 interface Props {
   params: Param[];
@@ -13,7 +14,7 @@ interface Param {
 }
 interface Model {
   paramValues: ParamValue[];
-  colors: Color[];
+  colors?: Color[];
 }
 interface ParamValue {
   paramId: number;
@@ -22,54 +23,91 @@ interface ParamValue {
 interface Color {
   value: string
 }
-/*
-  const params = [
+
+const paramsExample: Param[] = [
+  {
+    id: 1,
+    name: "Назначение",
+    type: 'string'
+  },
+  {
+    id: 2,
+    name: "Длина",
+    type: 'string'
+  },
+];
+const modelExample = {
+  paramValues: [
     {
-      id: 1,
-      name: "Назначение",
+      paramId: 1,
+      value: "повседневное",
     },
     {
-      id: 2,
-      name: "Длина",
+      paramId: 2,
+      value: "макси",
     },
-  ];
-  const model = {
-    paramValues: [
-      {
-        paramId: 1,
-        value: "повседневное",
-      },
-      {
-        paramId: 2,
-        value: "макси",
-      },
-    ],
-  };
-*/
-class ParamEditor extends React.Component<Props, State> {
+  ],
+};
+
+class ParamEditorInput extends React.Component<{ id: number, name: string, value: string, type: string, event: VoidFunction }> {
+  render() {
+    const valueId: number = this.props.id;
+    return(
+      <label className='flex items-center gap-x-[20px] my-[10px]'>
+          <span className='block w-3xs text-center'>{this.props.name}</span>
+          <input type={this.props.type} defaultValue={this.props.value} className='block w-3xs p-[4px] text-start bg-white border border-gray-300' placeholder={'Значение'} onChange={ () => this.props.event(event, valueId) }/>
+      </label>
+    )
+  }
+}
+
+class ParamEditor extends React.Component<Props, Model> {
+  state: Model = this.props.paramModel
   public getModel(): Model {
+    return this.state
   }
   
   render(){
+    const changeState = (e:React.FormEvent<HTMLInputElement>, id: number) : void => {
+      this.setState( (state: Model) => {
+        const newState:Model = {
+          paramValues: []
+        }
+        newState.paramValues = state.paramValues.map( (el) => {
+          if (el.paramId === id){
+            el.value = e.target.value
+          }
+          return el
+        })
+        return newState
+      })
+    }
+
+    const inputs: ReactElement[] = [];
+    for (const param of this.props.params){
+      const value = this.state.paramValues.find( (el) => el.paramId === param.id)?.value
+      inputs.push(<ParamEditorInput key={param.id} id={param.id} name={param.name} value={value} type={param.type} event={changeState} />)
+    }
     return (
       <div>
-        <label>
-          <span>Название</span>
-          <input value={'Значение'} />
-        </label>
+        {inputs}
+        <div className='my-[30px] text-center'>
+          <button className='px-[20px] py-[10px] border border-gray-300 hover:bg-gray-300 active:bg-gray-200' onClick={() => console.log('getModel()', this.getModel())}>getModel to console</button>
+        </div>
       </div>
     )
   }
 }
 
-const App:React.FunctionComponent = () => {
-  
-  return (
-    <>
-      <h1>Selsup test</h1>
-      <ParamEditor />
-    </>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div className='my-[40px] mx-auto max-w-3xl px-[20px]'>
+          <h1>Selsup test</h1>
+          <ParamEditor params={paramsExample} paramModel={modelExample}/>
+        </div>
+    );
+  }
 }
 
 createRoot(document.getElementById('root')!).render(
